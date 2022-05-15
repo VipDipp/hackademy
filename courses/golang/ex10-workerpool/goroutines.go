2fcaf158ead85b8a6ff67154d0366435bdc03aae
VipDipp <kuxaruk03@ukr.net>
2022-05-15 14:52:21 +0300

    done

--- courses/golang/ex10-workerpool/goroutines.go
+++ courses/golang/ex10-workerpool/goroutines.go
@@ -1,4 +1,34 @@
 package goroutines
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
 func Run(poolSize int) {
+	jobs := make(chan float64, poolSize)
+	i := 1
+	scan := bufio.NewScanner(os.Stdin)
+	for scan.Scan() {
+		input := string(scan.Bytes())
+		f, _ := strconv.ParseFloat(input, 64)
+		jobs <- f
+		if i <= poolSize {
+			go work(i, jobs)
+			i++
+		}
+	}
+	close(jobs)
+}
+
+func work(id int, jobs <-chan float64) {
+	fmt.Printf("worker:%d spawning\n", id)
+	for job := range jobs {
+		fmt.Printf("worker:%d sleep:%.1f\n", id, job)
+		time.Sleep(time.Duration(int(job*1000)) * time.Millisecond)
+	}
+	fmt.Printf("worker:%d stopping\n", id)
 }
