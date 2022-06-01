b4a8d4b1a4075cf152b938621c55c87fc0a1dc30
VipDipp <kuxaruk03@ukr.net>
2022-06-01 14:36:39 +0300

    barber fix

--- courses/golang/ex06-barbershop/barber.go
+++ courses/golang/ex06-barbershop/barber.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"math/rand"
 	"time"
 )
 
 type Barber struct {
-	state string
+	state      string
+	isSleeping bool
+}
+
+type BarberShop struct {
+	waitingRoom chan *Customer
+	arrived     chan *Customer
 }
 
 func NewBarber() (b *Barber) {
 	return &Barber{
-		state: "Sleeping",
+		state:      "Sleeping",
+		isSleeping: false,
+	}
+}
+
+func NewBarberShop(lobbySpace int, arriveSpace int) (bs *BarberShop) {
+	return &BarberShop{
+		waitingRoom: make(chan *Customer, lobbySpace),
+		arrived:     make(chan *Customer, arriveSpace),
 	}
 }
 
@@ -20,70 +34,85 @@ type Customer struct {
 	name string
 }
 
-func barber(b *Barber, waitingRoom chan *Customer, arrived chan *Customer) {
+func customerFlow(amount int, b *Barber, bs *BarberShop) {
+	names := []string{
+		"Bob",
+		"Sam",
+		"Danylo",
+		"Sasha",
+		"Matviy",
+		"Ivan",
+		"Jack",
+		"Lucifer",
+		"Arthas",
+		"Uter",
+	}
+	for i := 0; i < amount; i++ {
+		time.Sleep(time.Millisecond * time.Duration(rand.Intn(5)*10))
+		c := new(Customer)
+		c.name = names[rand.Intn(10)]
+		go customerEnter(c, b, bs)
+	}
+}
+
+func barberWork(b *Barber, bs *BarberShop) {
 	for {
 		select {
-		case c := <-waitingRoom:
+		case c := <-bs.waitingRoom:
 			b.state = "Cutting"
-			fmt.Printf(" %s ", c.name)
-			fmt.Println("")
+			fmt.Println("	Barber is cutting " + c.name)
 			time.Sleep(time.Millisecond * 100)
-
+			fmt.Println("	Barber done cutting " + c.name)
+		case c := <-bs.arrived:
+			b.state = "Cutting"
+			fmt.Println("	Barber is cutting " + c.name)
+			time.Sleep(time.Millisecond * 100)
+			fmt.Println("	Barber done cutting " + c.name)
 		default:
-			select {
-			case c := <-arrived:
-				b.state = "Cutting"
-				fmt.Printf(" %s ", c.name)
-				fmt.Println("")
-				time.Sleep(time.Millisecond * 100)
-			default:
-				time.Sleep(time.Millisecond * 100)
-				fmt.Printf(".")
-				b.state = "Sleeping"
+			if !b.isSleeping {
+				fmt.Println("		Barber go to sleep")
+				b.isSleeping = true
 			}
+			b.state = "Sleeping"
 		}
 	}
 }
 
-func customer(c *Customer, b *Barber, waitingRoom chan<- *Customer, arrived chan<- *Customer) {
+func customerEnter(c *Customer, b *Barber, bs *BarberShop) {
 	switch b.state {
 	case "Sleeping":
 		select {
-		case arrived <- c:
-			fmt.Println("WAKE UP")
-		default:
-			select {
-			case waitingRoom <- c:
-			default:
-				return
-			}
+		case bs.arrived <- c:
+			fmt.Println(c.name + " came, barber wake up")
+			b.isSleeping = false
 		}
 	case "Cutting":
 		select {
-		case waitingRoom <- c:
-			fmt.Println("New Customer")
+		case bs.waitingRoom <- c:
+			fmt.Println(c.name + " is waiting in lobby")
 		default:
-			fmt.Println("Full")
-			return
+			fmt.Println("		Lobby is full " + c.name + " is leaving but promised to return")
+			time.Sleep(time.Millisecond * 2000)
+			go customerEnter(c, b, bs)
 		}
 	}
 }
 
 func main() {
-	b := NewBarber()
-	waitingRoom := make(chan *Customer, 5)
-	arrived := make(chan *Customer, 1)
-	go barber(b, waitingRoom, arrived)
+	barber := NewBarber()
+	barberShop := NewBarberShop(5, 1)
+	go barberWork(barber, barberShop)
+	customerFlow(10, barber, barberShop)
 
-	for i := 0; i < 10; i++ {
-		time.Sleep(time.Millisecond * 50)
-		c := new(Customer)
-		c.name = "Customer" + strconv.Itoa(i)
-		go customer(c, b, waitingRoom, arrived)
-	}
 	time.Sleep(time.Millisecond * 1000)
-	c := new(Customer)
-	c.name = "late"
-	go customer(c, b, waitingRoom, arrived)
-	time.Sleep(time.Millisecond * 100000)
+	customerFlow(1, barber, barberShop)
+
+	for {
+
+	}
 }
+
+/*
+	to run you should initialize barber, create barbershop, and customer flow with amount of customers coming in barbershop,
+	also you can send one customer as well - initialize customer with name and run customerEnter with customer barbershop and barber
+*/
